0afb95f4283e4cd5450642ac5ea73f203295a510
B3njaaa <benj.valenzuelap@duocuc.cl>
2024-05-20 11:06:28 -0400

    Update saludo.py

--- 006D/saludo.py
+++ 006D/saludo.py
@@ -1,13 +1,4 @@
 op=0
-while (op!=3):
-    print("""¿El cris es?
-          1-Pollo
-          2-Wn
-          3-Wekito?""")
-    op=input("El cris es: ")
-    if op=1:
-            print("el cris es Pollo")
-    elif op=2:
-            print("el cris es Wn")
-    else:
-            print("el cris es wekito")
+while (op!=4):
+    print("4 Operaciones")
+    op=input("""Escoga una operacion que quiera 
